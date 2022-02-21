3ea5639ea206f16be79251e55bb922ce5046684c
Chris Gradwohl <15326707+cgradwohl@users.noreply.github.com>
2022-02-21 12:15:05 -0800

    save.

--- src/example.ts
+++ src/example.ts
@@ -1,11 +1,27 @@
-import * as AWSLambda from "aws-lambda";
+import {
+  Handler,
+  APIGatewayProxyEventV2,
+  APIGatewayProxyStructuredResultV2,
+} from "aws-lambda";
 
-export default async function handler(
-  event: AWSLambda.APIGatewayProxyEvent,
-  context: AWSLambda.Context
-): Promise<AWSLambda.APIGatewayProxyStructuredResultV2> {
+interface LambdaEvent extends Omit<APIGatewayProxyEventV2, "pathParameters"> {
+  pathParameters: { productId?: string; userId: string };
+}
+
+interface LambdaResult extends Omit<APIGatewayProxyStructuredResultV2, "body"> {
+  products: any;
+}
+
+export const handler: Handler<LambdaEvent, LambdaResult> = async (
+  event,
+  context
+): Promise<LambdaResult> => {
+  const { productId, userId } = event.pathParameters;
+
   return {
     statusCode: 200,
-    body: JSON.stringify({ event, context }),
+    products: JSON.stringify({ productId, userId, event, context }),
   };
-}
+};
+
+export default handler;
